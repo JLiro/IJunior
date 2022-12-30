463f1208fc6d0e6a59d24bc5dc27ad110ca76680
JLiro <0jayliro0@gmail.com>
2022-12-30 19:32:34 +0300

    Upd

--- Module01CSharp/Theme02ConditionalOperatorsAndLoops/Task05CurrencyConverter/Program.cs
+++ Module01CSharp/Theme02ConditionalOperatorsAndLoops/Task05CurrencyConverter/Program.cs
@@ -6,39 +6,32 @@ namespace Task05CurrencyConverter
     {
         static void Main()
         {
-            CurrencyConverter currencyConverter = new CurrencyConverter();
-            currencyConverter.Start();
-        }
-    }
+            const string UsdToRubСommand = "1";
+            const string UsdToEurСommand = "2";
+            const string EurToRubСommand = "3";
+            const string EurToUsdСommand = "4";
+            const string RubToUsdСommand = "5";
+            const string RubToEurСommand = "6";
+            const string ExitСommand = "7";
 
-    class CurrencyConverter
-    {
-        private const string UsdToRubСommand = "1";
-        private const string UsdToEurСommand = "2";
-        private const string EurToRubСommand = "3";
-        private const string EurToUsdСommand = "4";
-        private const string RubToUsdСommand = "5";
-        private const string RubToEurСommand = "6";
-        private const string ExitСommand = "7";
+            float usdBalance = 500;
+            float eurBalance = 500;
+            float rubBalance = 500;
 
-        private float usdBalance = 500;
-        private float eurBalance = 500;
-        private float rubBalance = 500;
+            float usdToRubCost = 10f;
+            float usdToEurCost = 0.5f;
+            float eurToRubCost = 20f;
+            float eurToUsdCost = 2f;
+            float rubToUsdCost = 0.1f;
+            float rubToEurCost = 0.2f;
 
-        private float usdToRubCost = 10f;
-        private float usdToEurCost = 0.5f;
-        private float eurToRubCost = 20f;
-        private float eurToUsdCost = 2f;
-        private float rubToUsdCost = 0.1f;
-        private float rubToEurCost = 0.2f;
-
-        public void Start()
-        {
+            float currencyCount;
             string userText;
 
-            bool isProgramRunning = true;
+            bool isProgramOpen = true;
+            bool isEnoughBalance;
 
-            while (isProgramRunning)
+            while (isProgramOpen)
             {
                 Console.WriteLine($"Ваш баланс счёта | {usdBalance} USD | {eurBalance} EUR | {rubBalance} RUB |" +
                                 $"\n" +
@@ -58,50 +51,116 @@ namespace Task05CurrencyConverter
                 {
                     case UsdToRubСommand:
                         Console.Write("Введите сколько USD хотите конвертировать в RUB: ");
-                        СonvertСurrency(ref usdBalance, ref rubBalance, usdToRubCost);
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                        isEnoughBalance = currencyCount <= usdBalance;
+                        if (isEnoughBalance)
+                        {
+                            usdBalance -= currencyCount;
+                            rubBalance += currencyCount * usdToRubCost;
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ошибка: Недостаточно средств на счёте!\n");
+                        }
                         break;
+
                     case UsdToEurСommand:
                         Console.Write("Введите сколько USD хотите конвертировать в EUR: ");
-                        СonvertСurrency(ref usdBalance, ref eurBalance, usdToEurCost);
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                        isEnoughBalance = currencyCount <= usdBalance;
+                        if (isEnoughBalance)
+                        {
+                            usdBalance -= currencyCount;
+                            eurBalance += currencyCount * usdToEurCost;
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ошибка: Недостаточно средств на счёте!\n");
+                        }
                         break;
+
                     case EurToRubСommand:
                         Console.Write("Введите сколько EUR хотите конвертировать в RUB: ");
-                        СonvertСurrency(ref eurBalance, ref rubBalance, eurToRubCost);
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                        isEnoughBalance = currencyCount <= eurBalance;
+                        if (isEnoughBalance)
+                        {
+                            eurBalance -= currencyCount;
+                            rubBalance += currencyCount * eurToRubCost;
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ошибка: Недостаточно средств на счёте!\n");
+                        }
                         break;
+
                     case EurToUsdСommand:
                         Console.Write("Введите сколько EUR хотите конвертировать в USD: ");
-                        СonvertСurrency(ref eurBalance, ref usdBalance, eurToUsdCost);
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                        isEnoughBalance = currencyCount <= eurBalance;
+                        if (isEnoughBalance)
+                        {
+                            eurBalance -= currencyCount;
+                            usdBalance += currencyCount * eurToUsdCost;
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ошибка: Недостаточно средств на счёте!\n");
+                        }
                         break;
+
                     case RubToUsdСommand:
                         Console.Write("Введите сколько RUB хотите конвертировать в USD: ");
-                        СonvertСurrency(ref rubBalance, ref usdBalance, rubToUsdCost);
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                        isEnoughBalance = currencyCount <= rubBalance;
+                        if (isEnoughBalance)
+                        {
+                            rubBalance -= currencyCount;
+                            usdBalance += currencyCount * rubToUsdCost;
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ошибка: Недостаточно средств на счёте!\n");
+                        }
                         break;
+
                     case RubToEurСommand:
                         Console.Write("Введите сколько RUB хотите конвертировать в EUR: ");
-                        СonvertСurrency(ref rubBalance, ref eurBalance, rubToEurCost);
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                        isEnoughBalance = currencyCount <= rubBalance;
+                        if (isEnoughBalance)
+                        {
+                            rubBalance -= currencyCount;
+                            eurBalance += currencyCount * rubToEurCost;
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ошибка: Недостаточно средств на счёте!\n");
+                        }
                         break;
-                    case ExitСommand: return;
+
+                    case ExitСommand:
+                        isProgramOpen = false;
+                        return;
                 }
-
-                isProgramRunning = userText.ToLower() != ExitСommand;
-            }
-        }
-
-        private void СonvertСurrency(ref float fromCurrencyBalance, ref float toCurrencyBalance, float currencyCost)
-        {
-            float currencyCount = Convert.ToSingle(Console.ReadLine());
-
-            bool isEnoughBalance = currencyCount <= fromCurrencyBalance;
-            if (isEnoughBalance)
-            {
-                fromCurrencyBalance -= currencyCount;
-                toCurrencyBalance += currencyCount * currencyCost;
-                Console.Clear();
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Ошибка: Недостаточно средств на счёте!\n");
             }
         }
     }
