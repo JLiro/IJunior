313c4d83aef0ca1c8644fdad710e106230dad755
JLiro <0jayliro0@gmail.com>
2023-01-09 00:37:15 +0300

    Upd

---
+++ Module01CSharp/Theme06OOP/Task01WorkingWithClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task01WorkingWithClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Игрок", 100, 100, 25);
+
+            player.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _health;
+        private int _armor;
+        private int _damage;
+
+        public Player(string name, int health, int armor, int damage)
+        {
+            _name = name;
+            _health = health;
+            _armor = armor;
+            _damage = damage;
+        }
+
+        public void ShowInfo()
+        {
+            Console.Write
+            (
+                $"Имя игрока: {_name}" +
+                $"\nЗдоровье игрока: {_health}" +
+                $"\nБроня игрока: {_armor}" +
+                $"\nУрон игрока: {_damage}"
+            );
+        }
+    }
+}
---
+++ Module01CSharp/Theme06OOP/Task02WorkingWithProperties/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task02WorkingWithProperties
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Renderer drow = new Renderer();
+            Player player = new Player(5, 2, '@');
+
+            drow.DrawPlayer(player.PoxitionX, player.PoxitionY, player.Skin);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Player
+    {
+        public char Skin { get; private set; }
+        public int PoxitionX { get; private set; }
+        public int PoxitionY { get; private set; }
+
+        public Player(int positionX, int positionY, char skin)
+        {
+            PoxitionX = positionX;
+            PoxitionY = positionY;
+            Skin = skin;
+        }
+    }
+
+    public class Renderer
+    {
+        public void DrawPlayer(int x, int y, char playerSymbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(playerSymbol);
+        }
+    }
+}
