63a3ab7145561e4655dbed09c0fddfb28ee0ba3b
JLiro <0jayliro0@gmail.com>
2023-02-16 20:47:00 +0300

    Upd

--- Module01CSharp/Theme06OOP/Task06Store/Program.cs
+++ Module01CSharp/Theme06OOP/Task06Store/Program.cs
@@ -15,9 +15,14 @@ namespace Task06Store
     {
         public Market()
         {
+            OpenMarket();
+        }
+
+        private void OpenMarket()
+        {
             const string CommandExit = "E";
 
-            string input  = String.Empty;
+            string input = String.Empty;
             string output = String.Empty;
 
             Seller seller = new Seller();
@@ -27,26 +32,23 @@ namespace Task06Store
             {
                 Console.WriteLine($"[ ПРОДАВЕЦ ] {seller.Coins} монет");
                 seller.ShowProducts();
+
                 Console.WriteLine();
+
                 Console.WriteLine($"[ ПОКУПАТЕЛЬ ] {player.Coins} монет");
                 player.ShowProducts();
 
-                Console.Write
-                (
-                     "\n" +
-                    $"Последнее событие: {output}" +
-                     "\n"
-                );
+                Console.WriteLine($"\nПоследнее событие: {output}");
 
                 Console.Write($"\nВведите номер товара для покупки или [{CommandExit}] для выхода: ");
                 input = Console.ReadLine();
 
                 if (seller.IsCheckProductAvailability(input, out Product product))
                 {
-                    if(player.Coins >= product.Price)
+                    if (player.Coins >= product.Price)
                     {
                         seller.Sell(product);
-                        player.Buy (product);
+                        player.Buy(product);
 
                         output = "Покупка успешно совершена";
                     }
@@ -65,21 +67,22 @@ namespace Task06Store
         }
     }
 
-    class Player
+    class Player : Persone
     {
-        private List<Product> _products = new List<Product>();
-        
         private Random _random = new Random();
 
         public Player()
         {
+            _products = new List<Product>();
+
             int minCoins = 0;
             int maxCoins = 1000;
 
             Coins = _random.Next(minCoins, maxCoins);
         }
 
-        public int Coins { get; private set; }
+        public override List<Product> _products { get; protected set; }
+        public override int Coins { get; protected set; }
 
         public void Buy(Product product)
         {
@@ -87,7 +90,47 @@ namespace Task06Store
 
             _products.Add(product);
         }
+    }
+    
+    class Seller : Persone
+    {
+        public Seller()
+        {
+            _products = new List<Product>()
+            {
+                new Product("one", 100),
+                new Product("bne", 300),
+                new Product("tne", 600)
+            };
+        }
 
+        public override List<Product> _products { get; protected set; }
+        public override int Coins { get; protected set; }
+
+        public void Sell(Product product)
+        {
+            Coins += product.Price;
+
+            _products.Remove(product);
+        }
+
+        public bool IsCheckProductAvailability(string input, out Product product)
+        {
+            product = null;
+
+            bool isProductAvailability = Int32.TryParse(input, out int productIndex) && productIndex >= 0 && productIndex < _products.Count;
+
+            if(isProductAvailability == true)
+            {
+                product = _products[productIndex];
+            }
+
+            return isProductAvailability;
+        }
+    }
+
+    abstract class Persone
+    {
         public void ShowProducts()
         {
             string output = "Инвентарь:";
@@ -106,79 +149,26 @@ namespace Task06Store
 
             Console.WriteLine(output);
         }
-    }
-    
-    class Seller
-    {
-        private List<Product> _products = new List<Product>();
 
-        public Seller()
-        {
-            _products = new List<Product>()
-            {
-                new Product("one", 100),
-                new Product("bne", 300),
-                new Product("tne", 600)
-            };
-        }
-
-        public int Coins { get; private set; }
-
-        public void Sell(Product product)
-        {
-            Coins += product.Price;
-
-            _products.Remove(product);
-        }
-
-        public void ShowProducts()
-        {
-            string output = "Инвентарь:";
-
-            if (_products.Count > 0)
-            {
-                for (int i = 0; i < _products.Count; i++)
-                {
-                    output += $"\n[{i}] {_products[i].GetInfo()}";
-                }
-            }
-            else
-            {
-                output = " Пусто";
-            }
-
-            Console.WriteLine(output);
-        }
-
-        public bool IsCheckProductAvailability(string input, out Product product)
-        {
-            product = null;
-
-            bool isProductAvailability = Int32.TryParse(input, out int productIndex) && productIndex >= 0 && productIndex < _products.Count;
-
-            if(isProductAvailability == true)
-            {
-                product = _products[productIndex];
-            }
-
-            return isProductAvailability;
-        }
+        public abstract List<Product> _products { get; protected set; }
+        public abstract int Coins { get; protected set; }
     }
 
     class Product
     {
+        private string _title;
+
         public Product(string title, int price)
         {
-            Title = title;
+            _title = title;
             Price = price;
         }
 
-        public string Title { get; set; }
-        public int Price { get; set; }
+        public int Price { get; private set; }
 
         public string GetInfo()
         {
-            return $"{Title} стоит {Price} монет";
+            return $"{_title} стоит {Price} монет";
         }
     }
 }
