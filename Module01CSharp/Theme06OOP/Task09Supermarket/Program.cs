18f8e5e43323dc27007d3844643c6ed983254090
JLiro <0jayliro0@gmail.com>
2023-05-18 15:02:52 +0300

    Upd

--- Module01CSharp/Theme06OOP/Task09Supermarket/Program.cs
+++ Module01CSharp/Theme06OOP/Task09Supermarket/Program.cs
@@ -1,275 +1,216 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Product
+namespace Task09Supermarket
 {
-    public Product(string name, double price)
+    public class Product
     {
-        Name = name;
-        Price = price;
-    }
-
-    public string Name { get; private set; }
-    public double Price { get; private set; }
-}
-
-public class Storage
-{
-    private List<Product> _products = new List<Product>();
-
-    public IReadOnlyList<Product> Products => _products;
-
-    public void AddProduct(Product product)
-    {
-        _products.Add(product);
-    }
-
-    public void RemoveProduct(Product product)
-    {
-        _products.Remove(product);
-    }
-
-    public double GetTotalPrice()
-    {
-        double totalPrice = 0;
-
-        foreach (var product in _products)
+        public Product(string name, double price)
         {
-            totalPrice += product.Price;
+            Name = name;
+            Price = price;
         }
 
-        return totalPrice;
-    }
-}
-
-public class Customer
-{
-    private Random _random = new Random();
-
-    public Customer(string name, double money)
-    {
-        Name = name;
-        Money = money;
+        public string Name { get; private set; }
+        public double Price { get; private set; }
     }
 
-    public string Name { get; private set; }
-    public double Money { get; set; }
-    public Storage Storage { get; } = new Storage();
-
-    public bool TryPay(double amount)
+    public class Storage
     {
-        if (Money < amount)
+        private List<Product> _products = new List<Product>();
+
+        public IReadOnlyList<Product> Products => _products;
+
+        public void AddProduct(Product product)
         {
-            return false;
+            _products.Add(product);
         }
 
-        Money -= amount;
-
-        return true;
-    }
-
-    public void RemoveRandomProduct()
-    {
-        if (Storage.Products.Count > 0)
+        public void RemoveProduct(Product product)
         {
-            int index = _random.Next(Storage.Products.Count);
-            Product product = Storage.Products[index];
-            Storage.RemoveProduct(product);
+            _products.Remove(product);
         }
-    }
-}
 
-public class Seller
-{
-    private List<Product> _products = new List<Product>();
-    private Queue<Customer> _queue = new Queue<Customer>();
-    private Random _random = new Random();
-
-    public void AddRandomProductToCustomer(Customer customer)
-    {
-        if (_products.Count > 0)
+        public double GetTotalPrice()
         {
-            int index = _random.Next(_products.Count);
-            Product product = _products[index];
-            customer.Storage.AddProduct(product);
-        }
-    }
+            double totalPrice = 0;
 
-    public void AddProduct(Product product)
-    {
-        _products.Add(product);
-    }
-
-    public void AddCustomer(Customer customer)
-    {
-        if (!_queue.Contains(customer))
-        {
-            _queue.Enqueue(customer);
-        }
-    }
-
-    public void ServeCustomers()
-    {
-        while (_queue.Count > 0)
-        {
-            Customer customer = _queue.Dequeue();
-
-            double totalPrice = customer.Storage.GetTotalPrice();
-
-            if (totalPrice == 0)
+            foreach (var product in _products)
             {
-                Console.WriteLine($"{customer.Name} не имеет товаров для покупки.");
+                totalPrice += product.Price;
             }
-            else
+
+            return totalPrice;
+        }
+    }
+
+    public class Customer
+    {
+        private Random _random = new Random();
+
+        public Customer(string name, double money)
+        {
+            Name = name;
+            Money = money;
+        }
+
+        public string Name { get; private set; }
+        public double Money { get; set; }
+        public Storage Storage { get; } = new Storage();
+
+        public bool TryPay(double amount)
+        {
+            if (Money < amount)
             {
-                bool canPay = customer.TryPay(totalPrice);
+                return false;
+            }
 
-                while (canPay == false && customer.Storage.Products.Count > 0)
-                {
-                    customer.RemoveRandomProduct();
+            Money -= amount;
+            return true;
+        }
 
-                    totalPrice = customer.Storage.GetTotalPrice();
-                    canPay = customer.TryPay(totalPrice);
-                }
+        public void RemoveRandomProducts(bool canPay)
+        {
+            while (canPay == false)
+            {
+                RemoveRandomProduct();
+                canPay = TryPay(Storage.GetTotalPrice());
+            }
+        }
 
-                if (canPay)
-                {
-                    Console.WriteLine($"{customer.Name} совершил покупку на сумму {totalPrice}.");
-                }
-                else
-                {
-                    Console.WriteLine($"{customer.Name} не имеет достаточно денег, чтобы заплатить.");
-                    Console.WriteLine($"{customer.Name} совершил покупку на сумму {totalPrice} после удаления некоторых продуктов.");
-                }
+        private void RemoveRandomProduct()
+        {
+            if (Storage.Products.Count > 0)
+            {
+                int index = _random.Next(Storage.Products.Count);
+                Product product = Storage.Products[index];
+                Storage.RemoveProduct(product);
             }
         }
     }
-}
 
-public class Supermarket
-{
-    private readonly List<Customer> _customers = new List<Customer>();
-    private readonly List<Product> _products = new List<Product>();
-    private Random _random = new Random();
-
-    public void Work()
+    public class Supermarket
     {
-        int minEach = 1;
-        int maxEach = 11;
+        private readonly List<Product> _products = new List<Product>();
+        private readonly Queue<Customer> _customers = new Queue<Customer>();
+        private Random _random = new Random();
 
-        AddProduct(new Product("Хлеб", 0.5));
-        AddProduct(new Product("Молоко", 0.5));
-        AddProduct(new Product("Яйца", 0.8));
-        AddProduct(new Product("Рыба", 0.9));
-
-        AddRandomCustomers();
-
-        foreach (Customer customer in _customers)
+        public void Work()
         {
-            int count = _random.Next(minEach, maxEach);
+            int minEach = 0;
+            int maxEach = 10;
 
-            AddRandomProductsToCustomer(customer, count);
-        }
+            AddProduct(new Product("Хлеб", 0.5));
+            AddProduct(new Product("Молоко", 0.5));
+            AddProduct(new Product("Яйца", 0.8));
+            AddProduct(new Product("Рыба", 0.9));
 
-        ServeCustomers();
+            AddRandomCustomers();
 
-        Console.ReadKey();
-    }
-
-    private void AddProduct(Product product)
-    {
-        _products.Add(product);
-    }
-
-    private void AddRandomProductsToCustomer(Customer customer, int count)
-    {
-        for (int i = 0; i < count; i++)
-        {
-            AddRandomProductToCustomer(customer);
-        }
-    }
-
-    private void AddRandomProductToCustomer(Customer customer)
-    {
-        if (_products.Count > 0)
-        {
-            int index = _random.Next(_products.Count);
-            Product product = _products[index];
-            customer.Storage.AddProduct(product);
-        }
-    }
-
-    private void AddCustomer(Customer customer)
-    {
-        _customers.Add(customer);
-    }
-
-    private void AddRandomCustomers()
-    {
-        double minMoney = 0.1;
-        double maxMoney = 3.5;
-
-        int minCustomer = 1;
-        int maxCustomer = 11;
-
-        int count = _random.Next(minCustomer, maxCustomer);
-
-        for (int i = 0; i < count; i++)
-        {
-            int customerID = i + 1;
-
-            Customer customer = new Customer($"Клиент {customerID}", nextDouble(_random, minMoney, maxMoney));
-            AddCustomer(customer);
-        }
-    }
-
-    private void ServeCustomers()
-    {
-        foreach (Customer customer in _customers)
-        {
-            double totalPrice = customer.Storage.GetTotalPrice();
-
-            if (totalPrice == 0)
+            foreach (Customer customer in _customers)
             {
-                Console.WriteLine($"{customer.Name} не имеет товаров для покупки.");
+                int count = _random.Next(minEach, maxEach);
+                AddRandomProductsToCustomer(customer, count);
             }
-            else
+
+            ServeCustomers();
+
+            Console.ReadKey();
+        }
+
+        private void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+        private void AddRandomProductsToCustomer(Customer customer, int count)
+        {
+            for (int i = 0; i < count; i++)
             {
+                AddRandomProductToCustomer(customer);
+            }
+        }
+
+        private void AddRandomProductToCustomer(Customer customer)
+        {
+            if (_products.Count > 0)
+            {
+                int index = _random.Next(_products.Count);
+                Product product = _products[index];
+                customer.Storage.AddProduct(product);
+            }
+        }
+
+        private void AddCustomer(Customer customer)
+        {
+            _customers.Enqueue(customer);
+        }
+
+        private void AddRandomCustomers()
+        {
+            double minMoney = 0.1;
+            double maxMoney = 3.5;
+
+            int minCustomer = 1;
+            int maxCustomer = 11;
+
+            int count = _random.Next(minCustomer, maxCustomer);
+
+            for (int i = 0; i < count; i++)
+            {
+                int customerID = i + 1;
+                Customer customer = new Customer($"Клиент {customerID}", NextDouble(minMoney, maxMoney));
+                AddCustomer(customer);
+            }
+        }
+
+        private void ServeCustomers()
+        {
+            while (_customers.Count > 0)
+            {
+                Customer customer = _customers.Dequeue();
+                double totalPrice = customer.Storage.GetTotalPrice();
+
+                if (totalPrice == 0)
+                {
+                    Console.WriteLine($"{customer.Name} не имеет товаров для покупки.\n");
+                    continue;
+                }
+
                 bool canPay = customer.TryPay(totalPrice);
 
                 if (canPay)
                 {
-                    Console.WriteLine($"{customer.Name} совершил покупку на сумму {totalPrice}.");
+                    Console.WriteLine($"{customer.Name} совершил покупку на сумму {totalPrice}.\n");
+                    continue;
                 }
-                else
+
+                customer.RemoveRandomProducts(canPay);
+
+                if (customer.Storage.Products.Count == 0)
                 {
-                    while (canPay == false && customer.Storage.Products.Count > 0)
-                    {
-                        customer.RemoveRandomProduct();
-
-                        totalPrice = customer.Storage.GetTotalPrice();
-                        canPay = customer.TryPay(totalPrice);
-                    }
-
-                    Console.WriteLine($"{customer.Name} не имеет достаточно денег, чтобы заплатить.");
-                    Console.WriteLine($"{customer.Name} совершил покупку на сумму {totalPrice} после удаления некоторых продуктов.");
+                    Console.WriteLine($"{customer.Name} не имеет достаточно денег, чтобы заплатить.\n");
+                    continue;
                 }
+
+                totalPrice = customer.Storage.GetTotalPrice();
+
+                Console.WriteLine($"{customer.Name} совершил покупку на сумму {totalPrice} после удаления некоторых продуктов.\n");
             }
+        }
+
+        private double NextDouble(double minValue, double maxValue)
+        {
+            return _random.NextDouble() * (maxValue - minValue) + minValue;
         }
     }
 
-    private double nextDouble(Random random, double minValue, double maxValue)
+    class Program
     {
-        return random.NextDouble() * (maxValue - minValue) + minValue;
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        Supermarket supermarket = new Supermarket();
-
-        supermarket.Work();
+        static void Main()
+        {
+            Supermarket supermarket = new Supermarket();
+            supermarket.Work();
+        }
     }
 }
